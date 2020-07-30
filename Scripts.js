abe5f2874f79774572b2c911bd2b1945bc2667c5
cazzbr <cazzbr@gmail.com>
2020-07-30 19:02:52 -0300

    Finalizado menu e adicionado linhas para ver conteudo

---
+++ Scripts.js
@@ -0,0 +1,42 @@
+function activate() {
+    var height = document.body.clientHeight;
+        home_pos = document.getElementById('home').getBoundingClientRect()['top'];
+        sobre_pos = document.getElementById('sobre').getBoundingClientRect()['top'];
+        post_pos = document.getElementById('post').getBoundingClientRect()['top'];
+        contato_pos = document.getElementById('contato').getBoundingClientRect()['top'];
+    if (home_pos <= 0 && sobre_pos > 0 ) {
+        document.getElementById("home_menu").classList.add('active');
+        document.getElementById("sobre_menu").classList.remove('active');
+        document.getElementById("post_menu").classList.remove('active');
+        document.getElementById("contato_menu").classList.remove('active');
+    }
+    else if (sobre_pos <= 0 && post_pos > 0 ) {
+        document.getElementById("home_menu").classList.remove('active');
+        document.getElementById("sobre_menu").classList.add('active');
+        document.getElementById("post_menu").classList.remove('active');
+        document.getElementById("contato_menu").classList.remove('active');
+    }
+    else if (post_pos <= 0 && contato_pos > 0 ) {
+        document.getElementById("home_menu").classList.remove('active');
+        document.getElementById("sobre_menu").classList.remove('active');
+        document.getElementById("post_menu").classList.add('active');
+        document.getElementById("contato_menu").classList.remove('active');
+    }
+    else if (contato_pos <= 0) {
+        document.getElementById("home_menu").classList.remove('active');
+        document.getElementById("sobre_menu").classList.remove('active');
+        document.getElementById("post_menu").classList.remove('active');
+        document.getElementById("contato_menu").classList.add('active');
+    }
+    else {
+        document.getElementById("home_menu").classList.remove('active');
+        document.getElementById("sobre_menu").classList.remove('active');
+        document.getElementById("post_menu").classList.remove('active');
+        document.getElementById("contato_menu").classList.remove('active');
+    }
+}
+
+window.addEventListener('load', activate);
+window.addEventListener('scroll', activate);
+window.addEvemtListener('resize', activate);
+
